2829fb92efad31aa300e2afe580ac031154ec12a
Rogue <rogue87@tuta.io>
2026-01-24 15:46:02 +0200

    add rust@1.92.0 to mise.toml and fix mise tasks scripts

--- mise-tasks/run-container.sh
+++ mise-tasks/run-container.sh
@@ -2,4 +2,4 @@
 
 cargo build
 
-docker run -it --rm -v "$(pwd)/target/debug/dotsy:/usr/local/bin/dotsy:Z dotsy-dev"
+docker run -it --rm -v $(pwd)/target/debug/dotsy:/usr/local/bin/dotsy:Z dotsy-dev
