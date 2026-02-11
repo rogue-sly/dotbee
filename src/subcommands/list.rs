97854779fac3aabd44757b18d5818451fffd41b2
Rogue <rogue87@tuta.io>
2026-02-11 22:44:51 +0200

    refactor: list subcommand
    not much to refactor other than removing the unnecessary sorting

--- src/subcommands/list.rs
+++ src/subcommands/list.rs
@@ -1,43 +1,38 @@
 use colored::Colorize;
 use context::Context;
-
 use indexmap::IndexMap;
 use std::error::Error;
 
 pub fn run(context: &Context) -> Result<(), Box<dyn Error>> {
+    // List global symlinks
     if let Some(global) = &context.config.global {
         println!("{}", "global".yellow().bold());
-        print_links(&global.links);
+        show_links(&global.links);
     }
 
-    let active_profile_name = context.state.active_profile.as_ref();
-
+    // List profiles symlinks and hightlight the active profile (if present)
+    let active_profile = context.state.active_profile.as_ref();
     if let Some(profiles) = &context.config.profiles {
         for (name, profile) in profiles {
-            let is_active_in_state = active_profile_name == Some(&name);
+            let is_active = active_profile == Some(&name);
 
-            let title = if is_active_in_state {
+            let title = if is_active {
                 format!("{} (active)", name).green().bold()
             } else {
                 name.bold()
             };
 
             println!("{}", title);
-            print_links(&profile.links);
+            show_links(&profile.links);
         }
-    } else if context.config.global.is_none() {
-        println!("No profiles or global links defined in config.");
     }
 
     Ok(())
 }
 
-fn print_links(links: &IndexMap<String, String>) {
-    let mut links_vec: Vec<_> = links.iter().collect();
-    links_vec.sort_by_key(|(k, _)| k.as_str());
-
-    for (i, (target, source)) in links_vec.iter().enumerate() {
-        let is_last = i == links_vec.len() - 1;
+fn show_links(links: &IndexMap<String, String>) {
+    for (i, (target, source)) in links.iter().enumerate() {
+        let is_last = i == links.len() - 1;
         let branch = if is_last { "└──" } else { "├──" };
         println!("{} {} -> {}", branch, target, source);
     }
